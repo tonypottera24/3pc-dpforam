337fbe4a697fe8ece5235fe53e7fc34d736aea9f
Po-Chu Hsu <tonypottera@gmail.com>
2021-12-26 13:04:21 +0800

    test

--- test/proto_test.cpp
+++ test/proto_test.cpp
@@ -29,11 +29,11 @@ int main(int argc, char *argv[]) {
         "port", po::value<uint>()->default_value(8080), "server port")(
         "next_party_ip", po::value<std::string>()->default_value("127.0.0.1"), "next party's ip")(
         "next_party_port", po::value<uint>()->default_value(8080), "next party's port")(
-        "log_n", po::value<uint>()->default_value(2), "number of data (log)")(
+        "log_n", po::value<uint>()->default_value(9), "number of data (log)")(
         "data_size", po::value<uint>()->default_value(8), "data size (bytes)")(
         "tau", po::value<uint>()->default_value(3), "tau, number of data included in a block (log)")(
         "log_ssot_threshold", po::value<uint>()->default_value(20), "ssot threshold (log)")(
-        "log_pseudo_dpf_threshold", po::value<uint>()->default_value(10), "pseudo dpf threshold (log)")(
+        "log_pseudo_dpf_threshold", po::value<uint>()->default_value(0), "pseudo dpf threshold (log)")(
         "threads", po::value<uint>()->default_value(1), "number of threads")(
         "iterations", po::value<uint>()->default_value(100), "number of iterations");
 
@@ -112,8 +112,8 @@ int main(int argc, char *argv[]) {
     // fprintf(stderr, "Initilizing PRG done. (%u, %u) (%u, %u)\n", offset[(party + 2) % 3], offset[(party + 2) % 3] + 16, offset[party], offset[party] + 16);
 
     uint64_t start_time = timestamp();
-    DPFORAM<BinaryData, BinaryData> dpf_oram = DPFORAM<BinaryData, BinaryData>(party, peer, n, data_size, tau, ssot_threshold, pseudo_dpf_threshold);
-    // DPFORAM<ZpData, BinaryData> dpf_oram = DPFORAM<ZpData, BinaryData>(party, peer, n, data_size, tau, ssot_threshold, pseudo_dpf_threshold);
+    // DPFORAM<BinaryData, BinaryData> dpf_oram = DPFORAM<BinaryData, BinaryData>(party, peer, n, data_size, tau, ssot_threshold, pseudo_dpf_threshold);
+    DPFORAM<ZpData, BinaryData> dpf_oram = DPFORAM<ZpData, BinaryData>(party, peer, n, data_size, tau, ssot_threshold, pseudo_dpf_threshold);
     uint64_t end_time = timestamp();
     fprintf(stderr, "Time to initilize DPF ORAM: %llu\n", end_time - start_time);
 
