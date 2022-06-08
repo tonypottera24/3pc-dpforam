06bb14a66284a7cf67cb16ec14b2fcb07cc724ec
Po-Chu Hsu <tonypottera@gmail.com>
2022-06-08 07:56:26 +0000

    benchmark pdpf

--- benchmark.py
+++ benchmark.py
@@ -2,11 +2,20 @@
 
 import argparse
 import subprocess
+import datetime
+
+BENCHMARK_KEY = [
+    "TOTAL_TIME",
+    "TOTAL_BANDWIDTH",
+    "KEY_TO_INDEX_TIME",
+    "KEY_TO_INDEX_BANDWIDTH",
+    "KEY_TO_INDEX_COLLISION"
+]
 
 
-def start_benchmark(proto_test_arg):
+def start_benchmark(proto_test_args):
     out = subprocess.run(
-        proto_test_arg, stderr=subprocess.PIPE)
+        proto_test_args, stderr=subprocess.PIPE)
     result = {}
     if out.returncode == 0:
         stderr = out.stderr.decode("utf-8").splitlines()
@@ -22,7 +31,6 @@ def start_benchmark(proto_test_arg):
                 result["KEY_TO_INDEX_COLLISION"] = int(log.split(" ")[1])
         for benchmark_key in BENCHMARK_KEY:
             print(f"{benchmark_key} {result[benchmark_key]}")
-        import datetime
         print(
             f"{datetime.timedelta(seconds=result['TOTAL_TIME']/1000*1.5)}")
         return result
@@ -30,6 +38,18 @@ def start_benchmark(proto_test_arg):
         print('error')
         print(out.stderr.decode("utf-8"))
         exit(1)
+
+
+def print_output(x_axis):
+    for benchmark_key in BENCHMARK_KEY:
+        print(benchmark_key)
+        for i, x in enumerate(x_axis):
+            if benchmark_key == "KEY_TO_INDEX_COLLISION":
+                y = round(results[benchmark_key][i] / pow(2, x) * 100, 2)
+            else:
+                y = round(results[benchmark_key][i] / 1000, 2)
+            print(f"({x}, {y})", end="")
+        print("\n")
 
 
 parser = argparse.ArgumentParser()
@@ -50,37 +70,29 @@ NEXT_PORT = 9000 + (PARTY + 1) % 3
 # LOG_N = range(10, 23)  # P256 - P256
 
 LOG_N = 25
-TAU = range(1, 20)
+# TAU = range(1, 20)
 
-# TAU = 5
+TAU = 5
 DATA_SIZE = 4
 # DATA_SIZE = 32
-# DATA_SIZE = range(4, 129, 4)
+# DATA_SIZE = range(4, 129, 8)
 SSOT_THRESHOLD = 0
-# PSEUDO_DPF_THRESHOLD = range(LOG_N)
-PSEUDO_DPF_THRESHOLD = 0
-
-BENCHMARK_KEY = [
-    "TOTAL_TIME",
-    "TOTAL_BANDWIDTH",
-    "KEY_TO_INDEX_TIME",
-    "KEY_TO_INDEX_BANDWIDTH",
-    "KEY_TO_INDEX_COLLISION"
-]
+PSEUDO_DPF_THRESHOLD = range(LOG_N)
+# PSEUDO_DPF_THRESHOLD = 5
 
 results = {}
 for benchmark_key in BENCHMARK_KEY:
     results[benchmark_key] = []
 
 # for logn in LOG_N:
-for tau in TAU:
-    # for pdpf in PSEUDO_DPF_THRESHOLD:
+# for tau in TAU:
+for pdpf in PSEUDO_DPF_THRESHOLD:
     # for data_size in DATA_SIZE:
     # print(f"logn = {logn}")
-    print(f"tau = {tau}")
-    # print(f"pdpf = {pdpf}")
+    # print(f"tau = {tau}")
+    print(f"pdpf = {pdpf}")
     # print(f"data_size = {data_size}")
-    proto_test_arg = [
+    proto_test_args = [
         "./bin/test/proto_test",
         "--party", str(PARTY),
         "--port", str(PORT),
@@ -89,27 +101,16 @@ for tau in TAU:
         "--log_n", str(LOG_N),
         "--data_size", str(DATA_SIZE),
         # "--data_size", str(data_size),
-        # "--tau", str(TAU),
-        "--tau", str(tau),
+        "--tau", str(TAU),
+        # "--tau", str(tau),
         "--log_ssot_threshold", str(SSOT_THRESHOLD),
-        "--log_pseudo_dpf_threshold", str(PSEUDO_DPF_THRESHOLD),
-        # "--log_pseudo_dpf_threshold", str(pdpf),
+        # "--log_pseudo_dpf_threshold", str(PSEUDO_DPF_THRESHOLD),
+        "--log_pseudo_dpf_threshold", str(pdpf),
     ]
-    result = start_benchmark(proto_test_arg)
+    result = start_benchmark(proto_test_args)
     for k, v in result.items():
         results[k].append(v)
     print()
 
-
-for benchmark_key in BENCHMARK_KEY:
-    print(benchmark_key)
-    for i, v in enumerate(TAU):
-        # for i, v in enumerate(LOG_N):
-        # for i, v in enumerate(PSEUDO_DPF_THRESHOLD):
-        # for i, v in enumerate(DATA_SIZE):
-        if benchmark_key == "KEY_TO_INDEX_COLLISION":
-            r = round(results[benchmark_key][i] / pow(2, v) * 100, 2)
-        else:
-            r = round(results[benchmark_key][i] / 1000, 2)
-        print(f"({v}, {r})", end="")
-    print("\n")
+# print_output(TAU)
+print_output(PSEUDO_DPF_THRESHOLD)
